bf5f2ae43b708e4e308a12e88a4b57137ce74435
Sivakumarpro <sivaexcel.10@gmail.com>
2022-09-28 14:22:02 +0530

    to danny

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -3,10 +3,11 @@ package org.test;
 public class Sample {
 
 	private void m1() {
-		System.out.println("1");
+		System.out.println("100");
 		
 	}
 	private void m2() {
 		System.out.println("2");
+		System.out.println("danny");
 	}
 }
